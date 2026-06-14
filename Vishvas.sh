73d7f522f9b1c8d19b00b02f786f2bc9cd5ff3e3
VishvasVaradaraju <vishwasyv5@gmail.com>
2026-06-14 12:17:05 +0530

    Create Vishvas.sh

---
+++ Vishvas.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=================================="
+echo " Google Arcade 2026"
+echo " Created by Vishvas Varadaraju"
+echo "=================================="
+
+echo "Script is working successfully!"
